12c9841416bf20c78b1a506370b9dc4b5406ef44
Matt Kline <matt@bitbashing.io>
2021-01-22 22:47:49 -0800

    Deploy: Fetch and hard reset
    Probably more resilient than pull

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 systemctl --user stop sqcobot.service
-echo "Going go sq-co-bot dir"
+echo "Going to sq-co-bot dir"
 cd /opt/sq-co-bot/
-echo "Git pull in working dir"
-git pull
+echo "Fetching from Git and resetting to the latest master"
+git fetch
+git reset --hard origin/master
 echo "Trying to start sqcobot service"
 systemctl --user start sqcobot.service
