2a0610b0cd648cecff577dc5b53a53dc989c6fa9
Matt Kline <matt@bitbashing.io>
2021-01-24 20:12:50 -0800

    Bash is hard

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "::::::::::: stopping sqcobot.service"
-systemctl --user stop sqcobot.service
-echo "::::::::::: cd to sq-co-bot dir"
+set -euxo pipefail
+
+systemctl --user stop sqcobot.service || true
 cd /opt/sq-co-bot/
-echo "::::::::::: Git fetch in working dir"
 git fetch
-echo "::::::::::: Git reset in working dir"
 git reset --hard origin/master
-echo "::::::::::: starting sqcobot.service"
-systemctl --user start sqcobot.service
+systemctl --user restart sqcobot.service
